673b0110c1ac6b10f733fd54308c0cffb7ab5d27
Sergey Korolev <bongrichard@rambler.ru>
2024-01-09 15:48:15 +0300

    Упростил функцию get_from_and_to

--- startPack/app_operation.py
+++ startPack/app_operation.py
@@ -4,12 +4,12 @@ def app_operation(operations_json):
     five_operations = functions.get_five_operations(operations_json)
     for operation in five_operations:
         date = functions.get_date_reverse(operation)
-        name, from_num, to_num, name_2 = functions.get_from_and_to(operation)
+        from_name, from_num_hide, to_num_hide, to_name = functions.get_from_and_to(operation)
         amount = operation['operationAmount']['amount']
         currency = operation['operationAmount']['currency']['name']
         print(f'\n{date} {operation['description']}')
-        if name != 0:
-            print(f'{name} {from_num} -> {name_2} {to_num}')
+        if from_name != 0:
+            print(f'{from_name} {from_num_hide} -> {to_name} {to_num_hide}')
         else:
-            print(f'{name_2} {to_num}')
+            print(f'{to_name} {to_num_hide}')
         print(f'{amount} {currency}')
--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,5 @@
 import json
 
-
 def get_five_operations(operations_json):
     with open(operations_json, encoding='utf-8') as f:
         list_operation = json.load(f)
@@ -9,44 +8,32 @@ def get_five_operations(operations_json):
         return five_last_operations
 
 def get_from_and_to(operation):
-    to_list = operation['to'].split()
-    if len(to_list) == 3:
-        to_num = to_list[2]
-        name_2 = f'{to_list[0]} {to_list[1]}'
+    to_num = operation['to'].split()[-1]
+    if len(to_num) == 20:
+        to_num_hide = grouper_account(to_num)
     else:
-        to_num = to_list[1]
-        name_2 = to_list[0]
-    if len(to_num) == 16:
-        num_2 = grouper_card(to_num)
-    else:
-        num_2 = grouper_account(to_num)
+        to_num_hide = grouper_card(to_num)
+    to_name = ' '.join(operation['to'].split()[:-1])
     if len(operation) > 6:
-        from_list = operation['from'].split()
-        if len(from_list) == 3:
-            from_num = from_list[2]
-            name = f'{from_list[0]} {from_list[1]}'
+        from_num = operation['from'].split()[-1]
+        if len(from_num) == 20:
+            from_num_hide = grouper_account(from_num)
         else:
-            from_num = from_list[1]
-            name = from_list[0]
-        if len(from_num) == 16:
-            num = grouper_card(from_num)
-            return [name, num, num_2, name_2]
-        else:
-            num = grouper_account(from_num)
-            return [name, num, num_2, name_2]
-    else:
-        return [0, 0, num_2, name_2]
+            from_num_hide = grouper_card(from_num)
+        from_name = ' '.join(operation['from'].split()[:-1])
+        return [from_name, from_num_hide, to_num_hide, to_name]
+    return [0, 0, to_num_hide, to_name]
 
 def grouper_card(iterable):
     list_ = []
     for i in range(0, 16):
-        if i < 6 or i > 10:
+        if i < 6 or i > 11:
             list_.append(iterable[i])
-            if i == 3 or i == 11:
+            if i == 3:
                 list_.append(' ')
         else:
             list_.append('*')
-            if i == 7:
+            if i == 7 or i == 11:
                 list_.append(' ')
     return ''.join(list_)
 
